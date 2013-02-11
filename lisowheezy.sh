ec87320383a1eeae50f654baf6e2fbc5dd5e555d
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2013-02-11 10:46:59 -0500

    Generating script to generate iso image

---
+++ lisowheezy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+lb clean --binary
+lb config -b iso -d wheezy --archive-areas "main contrib non-free" -a amd64
+lb config --packages-lists gnome-core 
+lb config --virtual-root-size 4000 
+lb config --repositories "ftp.us.debian.org/debian"
+lb config --mirror-binary http://ftp.us.debian.org/debian/
+#lb config --aptitude
+lb config --bootappend-live "locale=es_CO.UTF-8 keyb=es"
+#Aunque funciona cuando uno le pone el username con otro nombre, los datos quedan en un usuario user
+#lb config --bootappend-live "username=pupilo"
+#This tasks make the cd doesn't be created
+#lb config --tasks "spanish spanish-desktop spanish-gnome-desktop"
+lb config --debian-installer false
+lb build
